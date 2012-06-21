947e4fc091078f8e02d1400d76957c42478d36a5
jkosz <jan.koszewski@livingsocial.com>
2012-06-21 19:48:41 -0400

    beginnings of oauth with twitter

---
+++ spec/requests/tweet_requests_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe "Tweets" do
+  context "" do
+  end
+end
--- spec/requests/user_requests_spec.rb
+++ spec/requests/user_requests_spec.rb
@@ -2,9 +2,26 @@ require 'spec_helper'
 
 describe "User" do
   context "when visiting the home page" do
-    it "displays an 'Login with Twitter' button" do
+    before do
       visit root_url
+    end
+
+    it "displays an 'Login with Twitter' button" do
+      
       page.should have_link("Login with Twitter")
+    end
+
+    it "displays a 'Skip authentication' link" do
+      visit root_url
+      page.should have_link("Skip authentication")
+    end
+
+    context "when a user clicks 'Login with Twitter'" do
+      it "should redirect to a twitter authentication page" do
+        click_link('Login with Twitter')
+        page.should have_content "Username"
+        page.should have_content "Password"
+      end
     end
   end
 end
