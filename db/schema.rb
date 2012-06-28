071d53b0062c209169663b5daff4e6bf527630fa
jkosz <jan.koszewski@livingsocial.com>
2012-06-27 21:31:31 -0400

    correcting db schema after updating via migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120628003128) do
+ActiveRecord::Schema.define(:version => 20120628005521) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(:version => 20120628003128) do
   end
 
   add_index "deals", ["purchased"], :name => "index_deals_on_purchased"
+
+  create_table "tweet_responses", :force => true do |t|
+    t.string   "user_id"
+    t.string   "tweet_id"
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tweets", :force => true do |t|
     t.string   "content"
