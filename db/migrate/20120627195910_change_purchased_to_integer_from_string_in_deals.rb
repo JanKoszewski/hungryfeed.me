1ae246df6fec0675367ca1858d1d41ffec96e9f3
jkosz <jan.koszewski@livingsocial.com>
2012-07-02 10:10:20 -0400

    removing purchase string from change purchased migration

--- db/migrate/20120627195910_change_purchased_to_integer_from_string_in_deals.rb
+++ db/migrate/20120627195910_change_purchased_to_integer_from_string_in_deals.rb
@@ -1,6 +1,6 @@
 class ChangePurchasedToIntegerFromStringInDeals < ActiveRecord::Migration
   def change
-  	remove_column :deals, :purchased, :string
+  	remove_column :deals, :purchased
     add_column :deals, :purchased, :integer
   end
 end
