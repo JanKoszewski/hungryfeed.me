2de41ace7698b14aaa166c24b77b79030c1b4d85
jkosz <jan.koszewski@livingsocial.com>
2012-07-02 09:16:45 -0400

    adding redis to go to intializer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,11 @@
 require 'resque'
 
-Resque.redis = "localhost:6379:1"
+if Rails.env.production?
+	ENV["REDISTOGO_URL"] ||= "localhost:6379:1"
+	uri = URI.parse(ENV["REDISTOGO_URL"])
+	Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+else
+	Resque.redis = "localhost:6379:1"
+end
 
 Resque.enqueue(TwitterFeed)
