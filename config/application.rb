9e64f1b1d71e3218a1edc7d9f021689f0c24465c
jkosz <jan.koszewski@livingsocial.com>
2012-07-02 11:41:26 -0400

    turned asset precompliation to true in my application.rb file

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,6 @@ module Hungryfeed
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
