6cdc780d0d77d7d525742370366a1af24eacb68a
jkosz <jan.koszewski@livingsocial.com>
2012-07-03 05:03:06 -0400

    tweated my initializers for heroku deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Hungryfeed::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  ENV["REDISTOGO_URL"] = "redis://jkosz:621e5c14e1d92478eb8bf3dda7eea5f7@carp.redistogo.com:9134/"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -34,4 +36,6 @@ Hungryfeed::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.gem 'redis'
 end
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,12 +1,3 @@
 require 'resque'
-require 'open-uri'
-
-if Rails.env.staging? || Rails.env.production?
-  uri = URI.parse ENV['REDISTOGO_URL']
-  Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
-else
-	Resque.redis = "localhost:6379:1"
-end
-
 
 Resque.enqueue(TwitterFeed)
