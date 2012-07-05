cd97078c7ac1de056149f6fc411d4f715c6d2683
jkosz <jan.koszewski@livingsocial.com>
2012-07-05 08:34:15 -0400

    setting global REDIS to Resque.redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,7 +2,7 @@ require 'resque'
 
 if Rails.env.staging? || Rails.env.production?
   uri = URI.parse ENV['REDISTOGO_URL']
-  Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+  REDIS = Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
 else
 	Resque.redis = "localhost:6379:1"
 end
