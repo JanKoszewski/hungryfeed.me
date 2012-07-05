8fd7f0d57d79319a7aafe24c8676cf283d4ba448
jkosz <jan.koszewski@livingsocial.com>
2012-07-05 08:56:59 -0400

    added Resque.after_fork = Proc.new { ActiveRecord::Base.retrieve_connection} to resque initializer and resque rake task

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,7 +2,8 @@ require 'resque'
 
 if Rails.env.staging? || Rails.env.production?
   uri = URI.parse ENV['REDISTOGO_URL']
-  REDIS = Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+  Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+  Resque.after_fork = Proc.new { ActiveRecord::Base.retrieve_connection}
 else
 	Resque.redis = "localhost:6379:1"
 end
